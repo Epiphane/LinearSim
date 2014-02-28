26165ab508ee6f4a487233eace45e2e3a9bd9df3
Epiphane <exyphnos@gmail.com>
2014-02-28 09:42:10 -0800

    Adding LinearSim.c

---
+++ LinearSim.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Report.h"
+
+int main() {
+   double endValues[2];
+   int finalTime, numCells, cellID;
+   int driverPipe[2], tempPipe[2];
+
+   scanf("%lf %lf %d %d", endvalues, endValues + 1, &finalTime, &numCells);
+   
+   for(cellID = 1; cellID <= numCells; cellID ++) {
+      
+   }
+
+   return 0;
+}
