19373b5be51249ab67ba21a420c4bdc09c1a0535
Epiphane <exyphnos@gmail.com>
2014-03-01 00:58:54 -0800

    I think it closes all pipes correctly...............thiiiiiink.

--- LinearSim.c
+++ LinearSim.c
@@ -6,14 +6,23 @@
 #define PARAM_LENGTH 10
 #define MAX_PARAMS 8
 
-void makeIntParameter(char *param, char letter, int value);
-void makeDoubleParameter(char *param, double value);
+char *makeIntParameter(char letter, int value);
+char *makeDoubleParameter(double value);
 
 int main() {
    double endValues[2];
    int finalTime, numCells, cellID, argNum = 1;
-   int driverPipe[2], tempPipe[2];
+   int driverPipe[2];
+   int rightPipeOut[2], rightPipeIn[2];
+   int leftPipeOut[2], leftPipeIn[2];
    char *params[MAX_PARAMS], *strCursor;
+
+   // Create pipe for driver
+   pipe(driverPipe);
+
+   // Set rightPipeIn[0] to -1, so that we know that no cells have been initialized yet.
+   // rightPipeIn is first set up when we're making cell [numCells].
+   rightPipeIn[0] = -1;
 
    scanf("%lf %lf %d %d", endValues, endValues + 1, &finalTime, &numCells);
 
@@ -21,36 +30,91 @@ int main() {
    *params = malloc(strlen("Cell") + 1);
    strcpy(*params, "Cell");
 
-   for (cellID = 0; cellID < numCells; cellID++) {
+   // VISUAL DIAGRAM OF WHAT ITS GONNA END LIKE
+   // The rightmost cell is created FIRST
+   // Notes: LPO/LPI = leftPipeOut, leftPipeIn 
+   //        RPO/RPI = rightPipeOut, rightPipeIn
+   //
+   // Cell 0 I(RPI) <--- O(LPO) Cell 1 I(RPI) <--- O(LPO) Cell 2
+   //    |   O(RPO) ---> I(LPI)    |   O(RPO) ---> I(LPI)    |
+   //    v                         v                         v
+   // (LinearSim)             (LinearSim)              (LinearSim)
+   while (numCells--) {
       //Set cell ID number
-      makeIntParameter(params[argNum++], 'S', numCells);
+      params[argNum++] = makeIntParameter('D', numCells);
+      params[argNum++] = makeIntParameter('S', finalTime);
+      params[argNum++] = makeIntParameter('O', driverPipe[1]);
 
-      //If first cell
+      // If last cell (first one created, largest ID)
+      if(rightPipeIn[0] == -1) {
+         params[argNum++] = makeDoubleParameter(endValues[1]);
+      }
+      // Otherwise we have an input/output situation on the right
+      else {
+         params[argNum++] = makeIntParameter('O', rightPipeOut[1]);
+         params[argNum++] = makeIntParameter('I', rightPipeIn[0]);
+      }
+
+      // If first cell (ID: 0, last one created)
       if(!cellID) {
-         makeDoubleParameter(params[argNum++], endValues[0]);
-         makeIntParameter(params[argNum++], 'O', cellID + 1);
+         params[argNum++] = makeDoubleParameter(endValues[0]);
+      }
+      // Otherwise we have an input/output situation on the left too!
+      else {
+         // Create leftPipe
+         pipe(leftPipeOut);
+         pipe(leftPipeIn);
+
+         params[argNum++] = makeIntParameter('O', leftPipeOut[1]);
+         params[argNum++] = makeIntParameter('I', leftPipeIn[0]);
       }
 
-      //If last cell
-      if(cellID == numCells - 1) {
-         makeDoubleParameter(params[argNum++], endValues[1]);
-         makeIntParameter(params[argNum++], 'O', cellID - 1);
-      }
-      /*
-      while(temp) {
+      params[argNum] = NULL;
 
-      }*/
+      // Make a child and have it run Cell
+      if(fork() == 0) {
+         execv("Cell",params);
+         printf("Cell creation failed\n");
+      }
+
+      // Free rightPipe since the driver don't care
+      if(rightPipeIn[0] != -1) {
+         close(rightPipeIn[0]);
+         close(rightPipeIn[1]);
+         close(rightPipeOut[0]);
+         close(rightPipeOut[1]);
+      }
+
+      // Change pipes
+      // rightPipeOut ----> leftPipeIn
+      // rightPipeIn  <---- leftPipeOut 
+      rightPipeIn = leftPipeOut;
+      rightPipeOut = leftPipeIn;
    }
+
+   // Cells initialized: Free the write end of driverPipe from LinearSim
+   close(driverPipe[1]);
+
+   // TODO: Main code
+
+   // Close reader
+   close(driverPipe[0]);
 
    return 0;
 }
 
-void makeIntParameter(char *param, char letter, int value) {
-   param = malloc(PARAM_LENGTH);
+char *makeIntParameter(char letter, int value) {
+   char *param = malloc(PARAM_LENGTH);
+   
    sprintf(param, "%c%d", letter, value);
+
+   return param;
 }
 
-void makeDoubleParameter(char *param, double value) {
-   param = malloc(PARAM_LENGTH);
+char *makeDoubleParameter(double value) {
+   char *param = malloc(PARAM_LENGTH);
+   
    sprintf(param, "V%lf", value);
+
+   return param;
 }
